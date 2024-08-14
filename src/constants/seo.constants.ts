62f48cd1cabfe7f416f162da73c0f85370956610
Kostiantyn Nahula <kostyannagula@gmail.com>
2024-08-14 09:44:03 +0300

    Create base structure of the app

---
+++ src/constants/seo.constants.ts
@@ -0,0 +1,3 @@
+export const NO_INDEX_PAGE = { robots: { index: false, follow: false } };
+export const SITE_NAME = 'Nest shop';
+export const SITE_DESCRIPTION = 'Welcome to the nest shop - unique platform for buying and selling online';
