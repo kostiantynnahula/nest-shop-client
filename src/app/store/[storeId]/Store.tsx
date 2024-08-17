b6749ce9a7455db649bf57099d1dcbafb8540282
Kostiantyn Nahula <kostyannagula@gmail.com>
2024-08-17 16:26:54 +0300

    Implented statistic page

--- src/app/store/[storeId]/Store.tsx
+++ src/app/store/[storeId]/Store.tsx
@@ -4,12 +4,14 @@ import { Heading } from '@/components/ui/heading';
 
 import styles from './Store.module.scss';
 import { MainStatistics } from './statistics/main-statistics/MainStatistics';
+import { MiddleStatistics } from './statistics/middle-statistics/MiddleStatistics';
 
 export function Store() {
 	return (
 		<div className={styles.wrapper}>
 			<Heading title='Statistic' />
 			<MainStatistics />
+			<MiddleStatistics />
 		</div>
 	);
 }
--- src/app/store/[storeId]/statistics/middle-statistics/Overview.tsx
+++ src/app/store/[storeId]/statistics/middle-statistics/Overview.tsx
@@ -1,9 +1,72 @@
+import { Area, AreaChart, CartesianGrid, XAxis } from 'recharts';
+
+import { Card, CardContent, CardHeader, CardTitle } from '@/components/ui/card';
+import {
+	type ChartConfig,
+	ChartContainer,
+	ChartTooltip,
+	ChartTooltipContent
+} from '@/components/ui/chart';
+
+import { formatPrice } from '@/utils/string/format-price';
+
+import styles from './MiddleStatistics.module.scss';
 import { IMonthlySales } from '@/app/shared/types/statistics.interface';
+
+const chartConfig = {
+	value: {
+		label: 'Revenue',
+		color: '#3B82F6'
+	}
+} satisfies ChartConfig;
 
 interface OverviewProps {
 	data: IMonthlySales[];
 }
 
-export function Overview({}: OverviewProps) {
-	return <div>Overview Props</div>;
+export function Overview({ data }: OverviewProps) {
+	return (
+		<Card>
+			<CardHeader className={styles.header}>
+				<CardTitle>Revenue</CardTitle>
+			</CardHeader>
+			<CardContent>
+				<ChartContainer
+					className='aspect-auto h-[310px] w-full'
+					config={chartConfig}
+				>
+					<AreaChart
+						accessibilityLayer
+						data={data}
+						margin={{
+							left: 12,
+							right: 12
+						}}
+					>
+						<CartesianGrid vertical={false} />
+						<XAxis
+							dataKey='date'
+							tickLine={false}
+							axisLine={false}
+							tickMargin={8}
+						/>
+						<ChartTooltip
+							content={
+								<ChartTooltipContent
+									labelFormatter={formatPrice}
+									indicator='line'
+								/>
+							}
+						/>
+						<Area
+							dataKey='value'
+							type='natural'
+							fill='var(--color-value)'
+							stroke='var(--color-value)'
+						/>
+					</AreaChart>
+				</ChartContainer>
+			</CardContent>
+		</Card>
+	);
 }
